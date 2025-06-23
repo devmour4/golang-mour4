c7875f1fa7c93be519a333d1d819554ed27fa328
Kevin Moura <140131314+devmour4@users.noreply.github.com>
2025-06-24 00:05:57 +0100

    atualização

---
+++ calculator_go/calc.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+//utilize seus conhecimentos de Go até agora para implementar uma calculadora 
+//completa: a função de soma, subtração, adição e multiplicação e apresente os resultados corretos na tela.
+func main() {
+	fmt.Println("Hello, Go!")
+}
---
+++ calculator_go/calc_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+// padrão triple A - AAA
+// A - Arrange (Preparar)
+// A - Act (Rodar)
+// A - Assert (verificar as asserções)
+
+/*padrão exemplificado abaixo*/
