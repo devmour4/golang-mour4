16718574c3854f22ca9f027a699231df5fdd18a7
Kevin Moura <140131314+devmour4@users.noreply.github.com>
2025-06-24 20:58:34 +0100

    calculator_project

--- calculator_go/calc.go
+++ calculator_go/calc.go
@@ -6,5 +6,44 @@ import (
 //utilize seus conhecimentos de Go até agora para implementar uma calculadora 
 //completa: a função de soma, subtração, adição e multiplicação e apresente os resultados corretos na tela.
 func main() {
-	fmt.Println("Hello, Go!")
+	w := Sum(1, 2, 3)
+	x := Sub(5, 10)
+	y := Mult(10, 10)
+	z := Division(20)
+
+	fmt.Println(w, x, y, z)
+	
+}
+
+func Sum (i ...int) int {
+	total := 0
+	
+	for _, v := range i {
+		total += v
+	}
+	return total
+}
+func Sub (i	...int) int {
+	total := 0
+
+	for _, v := range i {
+		total = v - total
+	}
+	return total
+}
+func Mult (i ...int) int {
+	total := 1
+	
+	for _, v := range i {
+		total = total * v
+	}
+	return total
+}
+func Division (i ...int) int {
+	total := 1
+	
+	for _, v := range i {
+		total = v / total
+	}
+	return total
 }
--- calculator_go/calc_test.go
+++ calculator_go/calc_test.go
@@ -3,9 +3,94 @@ package main
 import (
 	"testing"
 )
+
 // padrão triple A - AAA
 // A - Arrange (Preparar)
 // A - Act (Rodar)
 // A - Assert (verificar as asserções)
 
 /*padrão exemplificado abaixo*/
+
+
+
+//****--Soma--****//
+func TestShouldSumCorrect (t *testing.T) {
+//arrange
+	teste := Sum(3, 2, 1)
+//Act
+	resultado := 6
+//Assert
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
+func TestShouldSumIncorrect(t *testing.T) {
+//Arrange
+	teste := Sum(3, 2, 1)
+//Act
+	resultado := 7
+//Assert
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
+
+//****--Sub--****//
+
+func TestShouldSubCorrect (t *testing.T) {
+//arrange
+	teste := Sub(10, 5)
+	resultado := -5
+
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
+func TestShouldSubIncorrect(t *testing.T) {
+	teste := Sub(10, 5)
+
+	resultado := 5
+
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
+
+//****--Multiplication--****//
+func TestShouldMultCorrect (t *testing.T) {
+//arrange
+	teste := Mult(10, 10)
+	resultado := 100
+
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
+func TestShouldMultIncorrect(t *testing.T) {
+	teste := Mult(10, 10)
+
+	resultado := 101
+
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
+//****--DIVISION--****/]
+func TestShouldDivisionCorrect (t *testing.T) {
+//arrange
+	teste := Division(20)
+	resultado := 20
+
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
+func TestShouldDivisionIncorrect(t *testing.T) {
+	teste := Division(20)
+
+	resultado := 21
+
+	if teste != resultado {
+		t.Error("valor esperado: ", resultado, "valor obtido: ", teste)
+	}
+}
